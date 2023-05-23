342f22a110a5883cf7e131ff58615500e3395487
jortiz24 <80806582+jortiz24@users.noreply.github.com>
2023-05-23 00:03:44 -0400

    Update Zigzag Conversion.py
    Comments added

--- Python/Zigzag Conversion.py
+++ Python/Zigzag Conversion.py
@@ -1,3 +1,5 @@
+##Leetcode Solution for problem #6
+
 class Solution(object):
     def convert(self, s, numRows):
         """
@@ -6,7 +8,7 @@ class Solution(object):
         :rtype: str
         """
 
-        if numRows == 1 or len(s) == 1:
+        if numRows == 1 or len(s) == 1: ## base case
             return s
         
         solution = ""
@@ -14,20 +16,20 @@ class Solution(object):
         for x in range(numRows):
             pos = x
             
-            counter = 0
+            counter = 0 # used to alternate when youre closer to the top row or bottom
             while (pos < len(s)) == True:
                 solution += s[pos]
                 print(solution)
 
                 if x == 0 or x == numRows-1:
+                    ## Slices string S when at the first or last row of the zigzag
                     pos += (numRows-1)*2
                 elif counter%2 == 0:
                     pos += abs((numRows-1)*2 - 2*x)
-                else:
+                else: ## these two condtionals slice depending whether youre closer to the top row or the bottom row (row 0 or row numRows-1)
                     pos += abs((numRows-1)*2 - 2*x - (numRows-1)*2)
                 print(pos)
                 counter += 1
         
         return solution
-
 
