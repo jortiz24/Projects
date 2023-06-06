4c54b0387d7f85f929b56038215b88218443a5a4
jortiz24 <80806582+jortiz24@users.noreply.github.com>
2023-06-06 03:14:30 -0400

    Update GasStations.rb
    Minor tweaks for readability and condensing code

--- Ruby/GasStations.rb
+++ Ruby/GasStations.rb
@@ -3,32 +3,39 @@
 # @param {Integer[]} cost
 # @return {Integer}
 def can_complete_circuit(gas, cost)
+    skip = 0
+    counter = 0
 
-  
+    (0..gas.length-1).each do |x|
 
+        if skip > x
+            x = skip
+        end
 
-    (0..gas.length).each do |x|
-        counter = x
+        
+        
+      
         cgas = 0
         ccost = 0
-        (0..gas.length).each do |y|
-            
-            cgas = cgas + gas[counter%gas.length]
-
-            break if cgas < ccost + cost[counter%gas.length]
-
-            ccost += cost[counter%gas.length]
-
-            
-            counter += 1
-
-           
-
-            if  counter % gas.length == x
+          
+        while true  
+            if counter > gas.length 
                 return x
             end
+
+            position = (x+counter)%gas.length
+
+            cgas += gas[position]
+            ccost += cost[position]
+
+            if ccost > cgas
+                skip = x
+                counter = 0
+                break
+            end
+            
+            counter += 1
         end
     end
-
     return -1
 end
