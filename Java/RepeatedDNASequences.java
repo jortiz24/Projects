9e0904023d1396bacd35b64ba222ee442832e822
jortiz24 <80806582+jortiz24@users.noreply.github.com>
2023-06-10 20:14:39 -0400

    Update RepeatedDNASequences.java

--- Java/RepeatedDNASequences.java
+++ Java/RepeatedDNASequences.java
@@ -3,34 +3,32 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
 
+        
+        
+        
+       
         List<String> output = new ArrayList<String>();
 
-        if(s.length() < 11)
-        {
-            return output ;
-        }
+        
         List<String> compare = new ArrayList<String>();
-      
+       
+
         for(int x = 0;x < s.length() - 9; x++){
           
             
             String seq = (s.substring(x,x+10));
 
-            if( compare.contains(seq))
+            if( !compare.contains(seq))
                 {
-                    if(output.contains(seq) == false)
-                    {
-                         output.add(seq)  ;
-                    }
-                   
-                  
+                    compare.add(seq);
                 } 
             else
                 {
-                    compare.add(seq);
+                    if(!output.contains(seq))
+                    {
+                        output.add(seq);
+                    }
                 }
-
-        
 
         }
 
